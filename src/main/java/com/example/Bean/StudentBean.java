086c97522ba77581d853cfe5c2b09c71bf73b649
liao <408411345@gms.tku.edu.tw>
2022-06-15 22:39:54 +0800

    add controller bean

---
+++ src/main/java/com/example/Bean/StudentBean.java
@@ -0,0 +1,15 @@
+package com.example.Bean;
+
+public class StudentBean {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
