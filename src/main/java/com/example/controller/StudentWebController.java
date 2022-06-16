958c71edf792cde17ae7a695fd12608e0254c374
liao <408411345@gms.tku.edu.tw>
2022-06-17 03:57:02 +0800

    use getter and setter deal with .csv

--- src/main/java/com/example/controller/StudentWebController.java
+++ src/main/java/com/example/controller/StudentWebController.java
@@ -1,23 +1,34 @@
 package com.example.controller;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class StudentWebController {
 	
 	@RequestMapping("/bean")
-	public void nameSpringBoot(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
-		convert(doPost(request,response));
+	public void nameSpringBoot(HttpServletRequest request,HttpServletResponse response,
+			@RequestBody convertCSV requestBean) throws ServletException, IOException {
+		String[] id=requestBean.getText1();
+		String[] name=requestBean.getText2();
+		String[] dep=requestBean.getText2();
+		String[] Class=requestBean.getText2();
+		int record=-1;
+		for (int i=0;i<19;i++) {
+			if(doPost(request,response)==name[i] ){
+				record=i;
+				break;
+			}
+		}
+		System.out.println(id[record]+" "+dep[record]+" "+Class[record]);
+
 	}
 	
 	protected String doPost(HttpServletRequest request,	HttpServletResponse response)
@@ -27,38 +38,5 @@ public class StudentWebController {
 			String username=request.getParameter("user") ;
 		return username;
 	}
-	private void convert(String name) {
-		String line = "";
-        final String delimiter = ",";
-        String[] text1=new String[19];
-        String[] text2=new String[19];
-        String[] text3=new String[19];
-        String[] text4=new String[19];
-        try
-        {
-            String filePath = "data_stu.csv";
-            FileReader fileReader = new FileReader(filePath);
-            int i=0;
-            BufferedReader reader = new BufferedReader(fileReader);
-            while ((line = reader.readLine()) != null)   //loops through every line until null found
-            {
-               String[] token = line.split(delimiter);// separate every token by comma
-               text1[i]=token[0];
-               text2[i]=token[1];
-               text3[i]=token[2];
-               text4[i]=token[3];
-               i++;
-            }
-            reader.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        for(int i=0;i<19;i++) {
-        	if(name==text2[i]) {
-        		System.out.println(text1[i]+""+text3[i]+""+text4[i]);
-        	}
-        }
-	}
+
 }
